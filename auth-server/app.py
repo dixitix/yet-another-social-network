00322ba942d6cd574934af4216cb13b800d35a9b
Vasya Pupkin <vasya@pupkin.ru>
2024-04-15 21:36:30 +0300

    add jwt from post-server branch

--- auth-server/app.py
+++ auth-server/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from database import add_user, update_user, authenticate_user, create_db
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['JWT_SECRET_KEY'] = '34567'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
+
+jwt = JWTManager(app)
 
 create_db()
 
@@ -25,19 +30,17 @@ def register():
         return jsonify({'error': 'Username and password are required'}), 400
 
 @app.route('/update', methods=['PUT'])
+@jwt_required()
 def update():
     data = request.json
-    username = data.get('username')
-    if username:
-        success = update_user(username, first_name=data.get('first_name'), last_name=data.get('last_name'),
-                              date_of_birth=datetime.strptime(data.get('date_of_birth'), '%Y-%m-%d').date() if data.get('date_of_birth') else None,
-                              email=data.get('email'), phone_number=data.get('phone_number'))
-        if success:
-            return jsonify({'message': 'User updated successfully'}), 200
-        else:
-            return jsonify({'error': 'User not found'}), 404
+    username = get_jwt_identity()
+    success = update_user(username, first_name=data.get('first_name'), last_name=data.get('last_name'),
+                            date_of_birth=datetime.strptime(data.get('date_of_birth'), '%Y-%m-%d').date() if data.get('date_of_birth') else None,
+                            email=data.get('email'), phone_number=data.get('phone_number'))
+    if success:
+        return jsonify({'message': 'User updated successfully'}), 200
     else:
-        return jsonify({'error': 'Username is required'}), 400
+        return jsonify({'error': 'User not found'}), 404
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -47,7 +50,8 @@ def login():
     if username and password:
         authenticated = authenticate_user(username, password)
         if authenticated:
-            return jsonify({'message': 'User authenticated successfully'}), 200
+            access_token = create_access_token(identity=username)
+            return jsonify({'message': 'User authenticated successfully'}, {'access_token=': access_token}), 200
         else:
             return jsonify({'error': 'Invalid credentials'}), 401
     else:
